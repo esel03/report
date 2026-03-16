ee878f470fae5dca05abac10065feec37ba64c05
egarmin_is <egarmin_is@magnit.ru>
2026-03-16 19:05:15 +0300

    add folder project

---
+++ project/pars_csv.py
@@ -0,0 +1,18 @@
+import csv
+from pathlib import Path
+
+
+class ReadCSV:
+    def pars(self, file_paths: list):
+        result_list = []
+        for file_path in file_paths:
+            path = Path(file_path)
+            if not (path.exists() and path.is_file()):
+                return None
+
+            with open(file_path, newline='') as csvfile:
+                result = csv.DictReader(csvfile)
+                for row in result:
+                    result_list.append(row)
+        return result_list
+
