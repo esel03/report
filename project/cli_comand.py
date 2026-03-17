770119fc7c141524ff2c5a7d76d66b88e723d2b4
egarmin_is <egarmin_is@magnit.ru>
2026-03-17 15:40:30 +0300

    add new change

--- project/cli_comand.py
+++ project/cli_comand.py
@@ -6,15 +6,16 @@ def read_cli() -> dict:
     parser = argparse.ArgumentParser(
                 prog='ProgramName',
                 description='What the program does',
-                epilog='Text at the bottom of help')
-    parser.add_argument('--file', nargs='+', help='List file for report')
+                epilog='Text at the bottom of help',
+                allow_abbrev=False)
+    parser.add_argument('--files', nargs='+', help='List file for report')
     parser.add_argument("--report")
     args = parser.parse_args()
 
-    for file_path in args.file:
+    for file_path in args.files:
         path = Path(file_path)
         if not (path.exists() and path.is_file()):
             raise FileNotFoundError
-    return {'file': args.file, 
+    return {'file': args.files, 
             'report': args.report}
 
